6ff7e009b513080ff85bc62306dcdf01c8d33f6c
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 01:31:05 -0400

    Initial commit

---
+++ lessons.py
@@ -0,0 +1,228 @@
+# -*- coding: utf-8 -*-
+from gettext import gettext as _
+import glib
+
+LESSONS = (
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 9 (Parte 1)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #8: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Cuando compartimos hacemos espacio para recibir más.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Aunque parezca que tenemos menos cuando compartimos, en realidad tenemos más.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • El compartir es una herramienta para sobrepasar nuestros comportamientos reactivos y así recibir la plenitud espiritual que verdaderamente queremos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Compartir es ser como la Luz.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Todos tenemos algo que compartir; podemos compartir cosas físicas y nuestras cualidades de la Luz.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿Cuál es un beneficio de compartir lo que tú has vivido?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Explica qué significa la siguiente frase para ti: “Mientras más das más recibes”.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Escribe tres acciones de compartir que hiciste esta semana. ¿Cómo te sentiste?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿Cómo te reta tu Oponente cuando quieres compartir? ¿Qué te dice?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿En qué situaciones te es difícil compartir?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 9 (Parte 2)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #8: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Cuando compartimos hacemos espacio para recibir más.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Aunque parezca que tenemos menos cuando compartimos, en realidad tenemos más.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • El compartir es una herramienta para sobrepasar nuestros comportamientos reactivos y así recibir la plenitud espiritual que verdaderamente queremos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Compartir es ser como la Luz.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Todos tenemos algo que compartir; podemos compartir cosas físicas y nuestras cualidades de la Luz.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿Cuál es un beneficio de compartir lo que tú has vivido?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Explica qué significa la siguiente frase para ti: “Mientras más das más recibes”.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Escribe tres acciones de compartir que hiciste esta semana. ¿Cómo te sentiste?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿Cómo te reta tu Oponente cuando quieres compartir? ¿Qué te dice?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • ¿En qué situaciones te es difícil compartir?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 10 (Parte 1)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #9: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('El verdadero compartir se trata de compartir con amor, para ayudar y sin expectativas.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Compartir con nosotros mismos y con otros conlleva cuidar de mi cuerpo, mi mente y mi Luz.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • El verdadero compartir es hacer lo que es mejor para mí, para los demás y para el mundo.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Al conocer mejor a nuestros amigos, nuestra familia y nuestro entorno, podemos aprender cómo compartir y ocuparnos de ellos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Para compartir verdaderamente, debemos hacer tres cosas:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       1. Compartir con amor')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       2. Compartir para ayudar')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       3. Compartir sin expectativas.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Palabras nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Compartir con amor: Elegir compartir con cariño, cuidado, y con una actitud positiva – no porque tenemos que hacerlo.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Compartir para ayudar: Identificar lo que la otra persona necesita y asegurarnos de que nuestras acciones no hagan daño; compartir no siempre significa dar.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Compartir sin expectativas: Compartir sin esperar recibir algo a cambio, saber que siempre vamos a tener plenitud espiritual.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • En tus propias palabras, explica qué significan para ti los tres componentes de compartir:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       1. Compartir con amor significa…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       2. Compartir para ayudar significa…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       3. Compartir sin expectativas significa…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Escribe las cosas de tu lista que sientas que necesitas hacer más.  Por ejemplo: Hacer más ejercicio, leer, expresar mis sentimientos cuando me siento enojado, etc.')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Dos Hermanos')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #9: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('El verdadero compartir se trata de compartir con amor, para ayudar y sin expectativas.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Compartir con nosotros mismos y con otros conlleva cuidar de mi cuerpo, mi mente y mi Luz.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • El verdadero compartir es hacer lo que es mejor para mí, para los demás y para el mundo.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Al conocer mejor a nuestros amigos, nuestra familia y nuestro entorno, podemos aprender cómo compartir y ocuparnos de ellos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Para compartir verdaderamente, debemos hacer tres cosas:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       1. Compartir con amor')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       2. Compartir para ayudar')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       3. Compartir sin expectativas.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Palabras nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Compartir con amor: Elegir compartir con cariño, cuidado, y con una actitud positiva – no porque tenemos que hacerlo.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Compartir para ayudar: Identificar lo que la otra persona necesita y asegurarnos de que nuestras acciones no hagan daño; compartir no siempre significa dar.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Compartir sin expectativas: Compartir sin esperar recibir algo a cambio, saber que siempre vamos a tener plenitud espiritual.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • En tus propias palabras, explica qué significan para ti los tres componentes de compartir:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       1. Compartir con amor significa…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       2. Compartir para ayudar significa…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       3. Compartir sin expectativas significa…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Escribe las cosas de tu lista que sientas que necesitas hacer más.  Por ejemplo: Hacer más ejercicio, leer, expresar mis sentimientos cuando me siento enojado, etc.')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lección 10 (Parte 2)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Regla #9: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('El verdadero compartir se trata de compartir con amor, para ayudar y sin expectativas.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Puntos clave:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Compartir con nosotros mismos y con otros conlleva cuidar de mi cuerpo, mi mente y mi Luz.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • El verdadero compartir es hacer lo que es mejor para mí, para los demás y para el mundo.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Al conocer mejor a nuestros amigos, nuestra familia y nuestro entorno, podemos aprender cómo compartir y ocuparnos de ellos.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Para compartir verdaderamente, debemos hacer tres cosas:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       1. Compartir con amor')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       2. Compartir para ayudar')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       3. Compartir sin expectativas.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Palabras nuevas:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Compartir con amor: Elegir compartir con cariño, cuidado, y con una actitud positiva – no porque tenemos que hacerlo.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Compartir para ayudar: Identificar lo que la otra persona necesita y asegurarnos de que nuestras acciones no hagan daño; compartir no siempre significa dar.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Compartir sin expectativas: Compartir sin esperar recibir algo a cambio, saber que siempre vamos a tener plenitud espiritual.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Reflexiona')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • En tus propias palabras, explica qué significan para ti los tres componentes de compartir:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       1. Compartir con amor significa…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       2. Compartir para ayudar significa…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('       3. Compartir sin expectativas significa…')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Escribe las cosas de tu lista que sientas que necesitas hacer más.  Por ejemplo: Hacer más ejercicio, leer, expresar mis sentimientos cuando me siento enojado, etc.')),
+)
---
+++ videos.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+VIDEOS = (
+    ("9.1.ogv",
+     _("El poder de compartir (Parte 1)"),
+     _('Continuamente animamos a nuestros hijos a compartir - compartir sus juguetes, su espacio, su bondad, a compartir cuando quieren y cuando no. Crecen viendo el compartir como lo "correcto" que hay que hacer, pero a menudo no comprendemos cómo compartir los ayuda. En esta lección, los niños descubrirán los beneficios espirituales de compartir, proporcionándoles la verdadera motivación para compartir más.'),
+         ),
+    ("9.5.ogv",
+     _("El poder de compartir (Parte 2)"),
+     _('Continuamente animamos a nuestros hijos a compartir - compartir sus juguetes, su espacio, su bondad, a compartir cuando quieren y cuando no. Crecen viendo el compartir como lo "correcto" que hay que hacer, pero a menudo no comprendemos cómo compartir los ayuda. En esta lección, los niños descubrirán los beneficios espirituales de compartir, proporcionándoles la verdadera motivación para compartir más.'),
+         ),
+    ("10.1.ogv",
+     _("Cómo compartir (Parte 1)"),
+     _("Aprendimos los beneficios espirituales de compartir y ahora, queremos compartir, pero, ¿cómo podemos compartir realmente? ¿Es el compartir siempre el acto físico de dar? Los niños pueden creer que compartir siempre significa ceder, pero a veces, significa decir que no. En esta lección, los niños explorarán el verdadero significado de compartir con ellos mismos y los demás."),
+    ),
+    ("10.2.ogv",
+     _("Dos Hermanos"),
+     _(""),
+    ),
+    ("10.6.ogv",
+     _("Cómo compartir (Parte 2)"),
+     _("Aprendimos los beneficios espirituales de compartir y ahora, queremos compartir, pero, ¿cómo podemos compartir realmente? ¿Es el compartir siempre el acto físico de dar? Los niños pueden creer que compartir siempre significa ceder, pero a veces, significa decir que no. En esta lección, los niños explorarán el verdadero significado de compartir con ellos mismos y los demás."),
+    ),
+)
